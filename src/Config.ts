763de6e7a6a416ccd035a669582e2b6544cec2fd
Tran Le <axe3182003@gmail.com>
2024-10-22 22:50:43 +0700

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 const config = {
     // apiUrl: 'https://localhost:7050/api'
-    apiUrl: 'https://healthappapi.scm.azurewebsites.net/api'
+    apiUrl: 'https://healthappapi.azurewebsites.net/api'
 }
 export default config;
